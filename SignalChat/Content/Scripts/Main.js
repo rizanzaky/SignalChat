1b7d2598836520259cd492f500a84733bce944a5
rizanzaky <rizan149@gmail.com>
2019-03-16 13:10:45 +0530

    add dummy chat message to chat window on send or hit enter

---
+++ SignalChat/Content/Scripts/Main.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+	$("#writer-footer > #message-send").click(function () {
+		publishPushAndClearMessage();
+	});
+
+	$("#writer-footer > #message-text").keypress(event => {
+		if (event.keyCode === 13) {
+			publishPushAndClearMessage();
+		}
+	});
+
+	function publishPushAndClearMessage() {
+		var message = $("#writer-footer > #message-text").val();
+		if (message === "") {
+			return;
+		}
+
+		// push
+		$("#chat-window").append(
+			'<div class="message my-message">' +
+				'<b>' + user + '</b><b class="message-time">02:30 am</b>' +
+				'<p>' + message + '</p>' +
+			'</div>'
+		);
+
+		// clear
+		$("#writer-footer > #message-text").val(null);
+	}
+});
